2484b0e7a62d3c1d9cb9e210fbc1e61b585c9024
Sweep76 <joshuapatrickchiu76@gmail.com>
2025-07-14 09:38:20 +0800

    feat: add AllDrink modal for initiating group drinking action

---
+++ modules/board/modals/AllDrink.tsx
@@ -0,0 +1,24 @@
+import { usePlayers } from '@/common/recoil/players';
+import { useModal } from '@/modules/modal';
+
+const AllDrink = () => {
+  const { allDrinks } = usePlayers();
+  const { closeModal } = useModal();
+
+  return (
+    <>
+      <h1 className="text-lg">Everyone is drinking!</h1>
+      <button
+        className="button mt-4 w-full"
+        onClick={() => {
+          allDrinks();
+          closeModal();
+        }}
+      >
+        młyn
+      </button>
+    </>
+  );
+};
+
+export default AllDrink;
