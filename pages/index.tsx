600daa09af2723dc973c35d89bda25c864f496f3
Sweep76 <joshuapatrickchiu76@gmail.com>
2025-07-16 14:26:36 +0800

    feat: create HomePage component with initial structure

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+import Home from '@/modules/home';
+
+const HomePage: NextPage = () => {
+  return <Home />;
+};
+
+export default HomePage;
+
+// ZROBIC KARTY DO KONCA
+// NAPRAWIC JAKIES BUGI JAK BEDA
+// EKRAN WYGRANEJ GRY
